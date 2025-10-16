5cb8d7c3153fc91f39d7879f6d8a07ee9be6fb3c
Doruk Bildibay <dbildibay@gmail.com>
2025-10-16 22:02:18 +0200

    remote player registers itself
    + on connect remote player registers with the GameMaster, no benefit atm.

--- Assets/Scripts/Miyav/Scripts/Core/GameMaster.cs
+++ Assets/Scripts/Miyav/Scripts/Core/GameMaster.cs
@@ -1,12 +1,11 @@
-using System.Collections.Generic;
-using System.Linq;
 using DorkyProductions.States;
+using Mirror;
 using UnityEngine;
 
 namespace DorkyProductions.Core
 {
     
-public class GameMaster : MonoBehaviour
+public class GameMaster : NetworkBehaviour
 {
     public static GameMaster Instance { get; private set; }
     
@@ -19,6 +18,7 @@ public class GameMaster : MonoBehaviour
     
     private Player localPlayer;
     private Player opponent;
+    
     
     void Awake()
     {
@@ -39,15 +39,7 @@ public class GameMaster : MonoBehaviour
 
         RegisterLocalPlayer("");
     }
-    void Start()
-    {
-        // Players need to be "connected" and 'set-up' BEFORE starting the game.
-        // _players.Add(new HumanPlayer(0, "Master Dork"));    
-        // _players.Add(new PlayerAI(1, "Dumb AI"));
-
-        // TODO: Remove this, should start when user presses 'Play' :))
-        // Invoke(nameof(StartNewGame), 1.5f);
-    }   
+ 
     private void Update()
     {
         CurrentState?.Update(this.context);
@@ -55,18 +47,38 @@ public class GameMaster : MonoBehaviour
 
     public void RegisterLocalPlayer(string playerName)
     {
-        localPlayer = new HumanPlayer(0, playerName);
+        if (localPlayer == null)
+        {
+            localPlayer = new HumanPlayer(0, playerName);
+        }
+        else
+        {
+            Debug.LogError("LocalPlayer already registered. Why are you trying to register again?");
+        }
     }
 
     public void RegisterOpponent(string playerName)
     {
-        opponent = new HumanPlayer(1, playerName);
+        if (opponent == null)
+        {
+            opponent = new HumanPlayer(1, playerName);
+        }
+        else
+        {
+            Debug.LogError("Opponent already registered. Why are you trying to register the opponent again?");
+        }
     }
 
     public void ChangeLocalPlayerName(string playerName)
     {
        localPlayer.DisplayName = playerName;
        Debug.Log($"Local player is now called: {playerName}");
+    }
+    
+    public void ChangeOpponentName(string playerName)
+    {
+        opponent.DisplayName = playerName;
+        Debug.Log($"Opponent is now called: {playerName}");
     }
     public void TransitionToState(IGameState newState)
     {
--- Assets/Scripts/PlayerNameDisplayer.cs
+++ Assets/Scripts/PlayerNameDisplayer.cs
@@ -11,12 +11,21 @@ public class PlayerNameDisplayer : NetworkBehaviour
     // The 'hook' will automatically call our update method whenever this changes.
     [SyncVar(hook = nameof(OnNameUpdated))]
     public string networkDisplayName = "Waiting..."; // Default name while loading
-
+    
     // This method is called automatically on ALL clients when 'networkDisplayName' changes value on the server.
     private void OnNameUpdated(string oldName, string newName)
     {
         // Update the UI text with the new name received from the server.
         playerNameText.text = newName;
+
+        if (!isLocalPlayer)
+        {
+            GameMaster.Instance.RegisterOpponent(newName);
+        }
+        else
+        {
+            Debug.Log("[low] LOCAL PLAYER skipping registration");
+        }
     }
 
     // This is called for every player object when they are first created on a client.
@@ -33,7 +42,7 @@ public class PlayerNameDisplayer : NetworkBehaviour
         // Get the name you chose in the menu (from your NameGenerator or a player prefs file).
         // I'm assuming your GameMaster stores this for you.
         string localName = GameMaster.Instance.GetLocalPlayer().DisplayName;
-        
+        playerNameText.text = localName;
         // Send a command to the server, telling it what our name is.
         CmdSetDisplayName(localName);
     }
