93d95f06e0b0203138760c9ffe0c1c7340e0c400
Doruk Bildibay <dbildibay@gmail.com>
2025-10-19 20:03:59 +0200

    switch to rpcs for all syncing.
    remove synclist events.
    board state is still a synclist, but events are not used.
    tiles match, move, swap and fall via client rpc's.

--- Assets/Scripts/ClientBoardVisualizer.cs
+++ Assets/Scripts/ClientBoardVisualizer.cs
@@ -27,9 +27,8 @@ public class ClientBoardVisualizer : MonoBehaviour
         tileDatabase.Initialize();
     }
 // REBUILT: Spawns the UI prefab
-    private void SpawnVisualTile(TileState state, int index)
+    public void SpawnVisualTile(TileState state, Vector2Int gridPos)
     {
-        Vector2Int gridPos = GetGridPosFromIndex(index);
         TileDefinitionSO def = tileDatabase.GetTileByType(state.tileType);
         if (def == null)
         {
@@ -39,7 +38,6 @@ public class ClientBoardVisualizer : MonoBehaviour
 
         // 1. Locally instantiate as a child of the board container
         GameObject tileGO = Instantiate(tileViewPrefab, gameBoard.boardContainer);
-        tileGO.name = $"Tile (ID: {state.uniqueID}, Type: {state.tileType})";
 
         // 2. Setup the RectTransform Spawn Position
         RectTransform rt = tileGO.GetComponent<RectTransform>();
@@ -56,16 +54,10 @@ public class ClientBoardVisualizer : MonoBehaviour
         
         // 5. Add to our dictionary for tracking
         _visualTiles[state.uniqueID] = tileView;
-    }
-    // --- Helper Methods ---
-
-    // Converts a 1D list index to a 2D grid position
-    private Vector2Int GetGridPosFromIndex(int index)
-    {
-        return new Vector2Int(index % GameBoard.BoardWidth, index / GameBoard.BoardHeight);
+        
+        Debug.Log($"[VIZ] Added tile {state.uniqueID} type: {state.ToString()} at ");
     }
     
-    // This is the core layout logic
     public Vector2 GetAnchoredPosition(Vector2Int gridPos)
     {
         Vector2 fullTileSize = gameBoard.tileViewSize + gameBoard.tileSpacing;
@@ -76,104 +68,48 @@ public class ClientBoardVisualizer : MonoBehaviour
 
         return new Vector2(x, y);
     }
-
-    public TileView GetTileViewAt(Vector2Int gridPos)
-    {
-        // This is inefficient, but simple.
-        // A 2D array of TileViews would be faster.
-        foreach (var tileView in _visualTiles.Values)
-        {
-            if (tileView.GridPosition == gridPos)
-            {
-                return tileView;
-            }
-        }
-        return null;
-    }
-
     public void OnBoardCleared()
     {
         _visualTiles.Clear();
     }
 
-    public void OnTileSet(int idx, TileState oldState, TileState newState)
+    public void RemoveTilesOnMatch(List<ushort> ids)
     {
-        Debug.Log($"[VIZ] {idx} now has {newState.uniqueID} type: {newState.tileType} at idx: {idx}");
-        // HandleTileMove(newState, idx);
-        
-        // Case 1: A tile is matched (cleared)
-        if (newState.IsEmpty())
+        foreach (ushort id in ids)
         {
-            if (_visualTiles.TryGetValue(oldState.uniqueID, out TileView tileToPop))
+            if (_visualTiles.TryGetValue(id, out TileView tileToPop))
             {
                 // Play a pop animation and destroy it
                 tileToPop.AnimatePop(); 
-                _visualTiles.Remove(oldState.uniqueID);
-            }
-        }
-        // Case 2: A tile moves (falls) or is spawned
-        else
-        {
-            TileView tileView;
-            if (_visualTiles.TryGetValue(newState.uniqueID, out tileView))
-            {
-                // --- This is a FALL ---
-                // The tile already exists, tell it to move
-                Vector2Int newGridPos = GetGridPosFromIndex(idx);
-                Vector2 newAnchoredPos = GetAnchoredPosition(newGridPos);
-                    
-                tileView.GridPosition = newGridPos;
-                tileView.MoveToPosition(newAnchoredPos, 0.2f); // Faster fall
-            }
-            else
-            {
-                // --- This is a NEW SPAWN ---
-                // This tile doesn't exist, spawn it
-                // We'll spawn it *above* the board and tell it to fall
-                SpawnVisualTile(newState, idx); 
-                // (You can modify SpawnVisualTile to animate it falling in)
+                _visualTiles.Remove(id);
             }
         }
     }
 
-    public void OnTileRemoved(int idx, TileState removedTile)
+    public void AnimateSwap(ushort firstTileID, ushort secondTileID)
     {
-        Debug.Log($"[VIZ] Removed (due to match or special effects) tile: {removedTile.uniqueID}");
-        Debug.LogError("[VIZ] Remove tile not implemented yet.");
+        TileView firstTileView = _visualTiles[firstTileID];
+        TileView secondTileView = _visualTiles[secondTileID];
+        
+        Vector2Int firstGridPos = firstTileView.GridPosition;
+        MoveTile(firstTileView, secondTileView.GridPosition);
+        MoveTile(secondTileView, firstGridPos);
     }
 
-    public void OnTileInserted(int idx, TileState insertedTile)
+    private void MoveTile(TileView tileToMove, Vector2Int newGridPos)
     {
-        // We might have already spawned this from PopulateInitialBoardVisuals
-        if (_visualTiles.ContainsKey(insertedTile.uniqueID))
-        {
-            Debug.Log($"[VIZ] The tile {insertedTile.uniqueID} already exists, due to init? ignoring INSERT request");
-            return;
-        }
-        
-        // This is a new tile falling in.
-        // For now, just spawn it. We can animate it "falling" later.
-        Debug.Log($"[VIZ]Inserted tile {insertedTile.uniqueID} type: {insertedTile.tileType} at idx: {idx}");
-        SpawnVisualTile(insertedTile, idx);
+        Vector2 newAnchoredPos = GetAnchoredPosition(newGridPos);
+        tileToMove.GridPosition = newGridPos;
+        tileToMove.MoveToPosition(newAnchoredPos, 0.2f);
     }
 
-    public void OnTileAdded(int idx, TileState addedTile)
-    {    
-        // TODO: Decide whether we consider Insertion & Add different events.
-        // so far, they lead to exactly the same code path.
-        // but insertion seems tobe related to "Refill board"
-        // whereas Add is about "Fill the board with initial tiles"
-        
-        // We might have already spawned this from PopulateInitialBoardVisuals
-        if (_visualTiles.ContainsKey(addedTile.uniqueID))
+    public void AnimateFall(TileState movedTile, Vector2Int toPos)
+    {
+        if (_visualTiles.TryGetValue(movedTile.uniqueID, out TileView tileView))
         {
-            Debug.Log($"[VIZ] The tile {addedTile.uniqueID} already exists, due to init? ignoring ADD request");
-            return;
+            Vector2 newAnchoredPos = GetAnchoredPosition(toPos);
+            tileView.GridPosition = toPos;
+            tileView.MoveToPosition(newAnchoredPos, 0.2f); // Faster fall
         }
-        
-        // This is a new tile falling in.
-        // For now, just spawn it. We can animate it "falling" later.
-        Debug.Log($"[VIZ] Added tile {addedTile.uniqueID} type: {addedTile.tileType} at idx: {idx}");
-        SpawnVisualTile(addedTile, idx);
     }
 }
--- Assets/Scripts/GameBoard.cs
+++ Assets/Scripts/GameBoard.cs
@@ -80,36 +80,49 @@ public class GameBoard : NetworkBehaviour
             boardState.Add(GenerateNewTile());
         }
     }
+    
     public override void OnStartClient()
     {
-        // Add handlers for SyncList Actions
-        boardState.OnAdd += OnItemAdded;
-        boardState.OnInsert += OnItemInserted;
-        boardState.OnSet += OnItemSet;
-        boardState.OnRemove += OnItemRemoved;
-        boardState.OnClear += OnListCleared;
+        // TODO: replace with an RPC that sends all 25 tile information as "Setup board"
+
+        if (!NetworkServer.active)
+        {
+            // Not the Host client.
+            // Cmd
+        }
 
         // For Clients, List is populated before handlers are wired up so we
         // need to manually invoke OnAdd for each element.
-        for (int i = 0; i < boardState.Count; i++)
-            boardState.OnAdd.Invoke(i);
+        // for (int i = 0; i < boardState.Count; i++)
+        //     boardState.OnAdd.Invoke(i);
+       SetupBoard();
+        
     }
-
+    // This is the setup event. It is ONLY called on this one client.
+    [TargetRpc]
+    public void TargetSetupGame(string message)
+    {
+        // --- YOUR SETUP LOGIC GOES HERE ---
+        Debug.Log($"Received setup message from server: {message}");
+        // e.g., StartMatchCountdown();
+    }
+    private void SetupBoard()
+    {
+        for (int i = 0; i < boardState.Count; i++)
+        {
+            var tile = boardState[i];
+            _visualizer.SpawnVisualTile(tile, GetGridPos(i));
+        }
+    }
     public override void OnStopClient()
     {
         // Remove handlers when client stops
-        boardState.OnAdd -= OnItemAdded;
-        boardState.OnInsert -= OnItemInserted;
-        boardState.OnSet -= OnItemSet;
-        boardState.OnRemove -= OnItemRemoved;
-        boardState.OnClear -= OnListCleared;
-        // namesList.OnChange -= OnListChanged;
+        // boardState.OnClear -= OnListCleared;
     }
 
-    private void OnItemAdded(int idx) => _visualizer.OnTileAdded(idx, boardState[idx]);
-    private void OnItemSet(int idx, TileState oldState) => _visualizer.OnTileSet(idx, oldState, boardState[idx]);
-    private void OnItemRemoved(int idx, TileState removedTile) => _visualizer.OnTileRemoved(idx, removedTile);
-    private void OnItemInserted(int idx) => _visualizer.OnTileInserted(idx, boardState[idx]);
+    // private void OnItemSet(int idx, TileState oldState) => _visualizer.OnTileSet(idx, oldState, boardState[idx]);
+    // private void OnItemRemoved(int idx, TileState removedTile) => _visualizer.OnTileRemoved(idx, removedTile);
+    // private void OnItemInserted(int idx) => _visualizer.OnTileInserted(idx, boardState[idx]);
     private void OnListCleared() => _visualizer.OnBoardCleared();
 
     private TileState GenerateNewTile()
@@ -121,24 +134,16 @@ public class GameBoard : NetworkBehaviour
         };
     }
     //
-    // [ClientRpc]
-    // private void RpcAnimateSwap(Vector2Int posA, Vector2Int posB)
-    // {
-    //     // Called on ALL clients.
-    //     // (The client who initiated the move can just ignore this)
-    //     if (isLocalPlayer) return; // Example of ignoring if you're the one
-    //     
-    //     // _visualizer.AnimateSwap(posA, posB);
-    // }
-    //
-    // [ClientRpc]
-    // private void RpcAnimateMatch(List<ushort> matchedTileIDs)
-    // {
-    //     // Called on ALL clients.
-    //     // _visualizer.AnimateMatch(matchedTileIDs);
-    // }
-    //
-    
+    [ClientRpc]
+    private void RpcAnimateSwap(ushort firstTileID, ushort secondTileID)
+    {
+        // TODO: Consider if client side prediction is necessary, disabled for now.
+        // Called on ALL clients.
+        // (The client who initiated the move can just ignore this)
+        // if (isLocalPlayer) return; // Example of ignoring if you're the one
+        _visualizer.AnimateSwap(firstTileID, secondTileID);
+    }
+
     [Server]
     public bool ProcessPlayerSwap(NetworkConnectionToClient sender, Vector2Int posA, Vector2Int posB)
     {
@@ -199,7 +204,7 @@ public class GameBoard : NetworkBehaviour
     [Server]
     private bool AttemptSwapAndProcessBoard(Vector2Int posA, Vector2Int posB)
     {
-        // --- 1. Perform the initial swap ---
+        // --- 1. Perform the swap ---
         int indexA = GetIndex(posA);
         int indexB = GetIndex(posB);
         TileState stateA = boardState[indexA];
@@ -207,7 +212,7 @@ public class GameBoard : NetworkBehaviour
 
         boardState[indexA] = stateB;
         boardState[indexB] = stateA;
-
+        RpcAnimateSwap(stateA.uniqueID, stateB.uniqueID);
         
         // Check if this swap caused a match
         // We only need to check the rows/cols of the two tiles we moved
@@ -233,8 +238,7 @@ public class GameBoard : NetworkBehaviour
                 }
             }
 
-            // --- D. ARRANGE PHASE (Let tiles fall) ---
-            // SimulateTileFall();
+            SimulateTileFall();
 
             // --- E. REFILL PHASE (Refill the board) ---
             // RefillBoard();
@@ -352,17 +356,22 @@ public class GameBoard : NetworkBehaviour
                 // modify the board again. Be careful of recursive loops!
                 // For now, let's keep it simple.
             }
-            RpcApplyMatchEffect(match);
+            
+            Debug.Log($"Matched: {match.matchCount} of {match.ToString()}");
+
+            var ids = new List<ushort>();
+            foreach (var pos in match.positions)
+            {
+                ids.Add(GetTileAt(pos).uniqueID);
+            }
+            RpcApplyMatchEffect(ids);
         }
     }
 
     [ClientRpc]
-    private void RpcApplyMatchEffect(MatchData matchData)
+    private void RpcApplyMatchEffect(List<ushort>ids)
     {
-        Debug.Log($"Matched: {matchData.matchCount} of {matchData.ToString()}");
-        
-        // TODO: Notify visualizer of the match.
-        // _visualizer.
+        _visualizer.RemoveTilesOnMatch(ids);
     }
 
     [Server]
@@ -377,13 +386,15 @@ public class GameBoard : NetworkBehaviour
                     // ...look for the first non-empty tile *above* it
                     for (int yAbove = y + 1; yAbove < BoardHeight; yAbove++)
                     {
-                        if (!GetTileAt(new Vector2Int(x, yAbove)).IsEmpty())
+                        TileState tileToMove = GetTileAt(new Vector2Int(x, yAbove));
+                        if (!tileToMove.IsEmpty())
                         {
                             // We found one! Move it down.
-                            Debug.Log($"Move tile ({x},{y}) to ({x},{yAbove})");
-                            boardState[GetIndex(x, y)] = GetTileAt(new Vector2Int(x, yAbove));
+                            Debug.Log($"Move tile ({x},{yAbove}) to ({x},{y})");
+                            boardState[GetIndex(x, y)] = tileToMove;
                             boardState[GetIndex(x, yAbove)] = TileState.Empty;
-                            
+
+                            RpcMoveTile(new Vector2Int(x, y), tileToMove);
                             // Break the inner 'yAbove' loop to continue
                             // checking the *current* 'y' position again.
                             break; 
@@ -393,7 +404,29 @@ public class GameBoard : NetworkBehaviour
             }
         }
     }
+    /// <summary>
+    /// This runs on ALL clients (including the host-client)
+    /// to tell them about a tile move.
+    /// </summary>
+    [ClientRpc]
+    private void RpcMoveTile(Vector2Int toPos, TileState movedTile)
+    {
+        // 1. Update the LOCAL board state for PURE clients.
+        // The host-client (isServer == true) already updated its state
+        // in the [Server] method, so it must skip this step.
+        // if (!isServer)
+        // {
+        //     boardState[GetIndex(toPos.x, toPos.y)] = movedTile;
+        //     boardState[GetIndex(fromPos.x, fromPos.y)] = TileState.Empty;
+        // }
 
+        // 2. Trigger visuals/animations for EVERYONE.
+        // Both pure-clients and the host-client need to see the tile move.
+        Debug.Log($"CLIENT: Animating tile to {toPos}");
+        _visualizer.AnimateFall(movedTile, toPos);
+        // e.g., StartCoroutine(AnimateTileFall(fromPos, toPos));
+    }
+    
     [Server]
     private void RefillBoard()
     {
@@ -416,10 +449,29 @@ public class GameBoard : NetworkBehaviour
 
     // --- COORDINATE & STATE HELPERS ---
 
-    private int GetIndex(Vector2Int pos) => (pos.y * BoardWidth) + pos.x;
+    public Vector2Int GetGridPos(int idx)
+    {
+        int x = idx % BoardHeight;
+        int y = idx / BoardHeight;
+        return new Vector2Int(x, y);
+    }
+    public int GetIndex(Vector2Int pos) => (pos.y * BoardWidth) + pos.x;
     private int GetIndex(int x, int y) => (y * BoardWidth) + x;
     private TileState GetTileAt(Vector2Int pos) => boardState[GetIndex(pos)];
-    
+
+    public TileState GetTile(ushort id)
+    {
+        // Use FindIndex, which is safe
+        int index = boardState.FindIndex(t => t.uniqueID == id);
+
+        if (index == -1)
+        {
+            // Not found, so return your safe "Empty" struct
+            Debug.LogError($"Tile with id: {id} not found. wtf?");
+            return TileState.Empty;
+        }
+        return boardState[index];
+    }
     private bool IsPosInBounds(Vector2Int pos)
     {
         return pos.x >= 0 && pos.x < BoardWidth &&
