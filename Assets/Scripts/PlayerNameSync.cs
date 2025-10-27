7c81e5132ab74a201bb6bbacb6195e919c0c000b
Doruk Bildibay <dbildibay@gmail.com>
2025-10-27 21:25:51 +0100

    fix missing ref
    move ref set to awake

--- Assets/Scripts/PlayerNameSync.cs
+++ Assets/Scripts/PlayerNameSync.cs
@@ -1,21 +1,16 @@
 using Mirror;
-using TMPro;
 using UnityEngine;
 
 namespace DorkyProductions
 {
     
-public class PlayerNameDisplayer : NetworkBehaviour
+public class PlayerNameSync : NetworkBehaviour
 {
-    [SerializeField] private TextMeshProUGUI playerNameText;
-
-    // This is the variable that will be synchronized across the network.
-    // The 'hook' will automatically call our update method whenever this changes.
     [SyncVar(hook = nameof(OnNameUpdated))]
-    public string networkDisplayName = "Waiting..."; // Default name while loading
+    private string networkDisplayName = "...";
 
     private PlayerAvatarUIController _avatarUIController;
-    private void Start()
+    private void Awake()
     {
         _avatarUIController = FindAnyObjectByType<PlayerAvatarUIController>();
         if (_avatarUIController == null)
@@ -27,22 +22,12 @@ public class PlayerNameDisplayer : NetworkBehaviour
     // This method is called automatically on ALL clients when 'networkDisplayName' changes value on the server.
     private void OnNameUpdated(string oldName, string newName)
     {
-        playerNameText.text = newName;
         if (!isLocalPlayer)
         {
             _avatarUIController.SetOpponentPlayerNameText(newName);
         }
     }
-
-    // This is called for every player object when they are first created on a client.
-    public override void OnStartClient()
-    {
-        // Set the initial name. This ensures that when a new player joins,
-        // everyone sees their default name immediately. The hook will update it shortly after.
-        playerNameText.text = networkDisplayName;
-    }
-
-    // This is called ONLY for the player that you control.
+    
     public override void OnStartLocalPlayer()
     {
         string localName = PlayerNameGenerator.GetChosenName();
@@ -52,18 +37,16 @@ public class PlayerNameDisplayer : NetworkBehaviour
         }
 
         _avatarUIController.SetLocalPlayerNameText(localName);
-        playerNameText.text = localName;
         CmdSetDisplayName(localName);
     }
 
-    // This [Command] is sent from your client to the server.
     [Command]
-    private void CmdSetDisplayName(string name)
+    private void CmdSetDisplayName(string playerName)
     {
         // The server receives the name and sets the SyncVar.
         // As soon as this line runs on the server, Mirror will send the new name
         // to all clients, which will trigger the 'OnNameUpdated' hook on each client.
-        networkDisplayName = name;
+        networkDisplayName = playerName;
     }
 }
 
